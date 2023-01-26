7e6d8c0dff97f192764b96594c05eeaf0552c2db
Hyun Yi <hy00un__@naver.com>
2023-01-26 22:46:36 +0900

    Chapter 04 완료

--- src/main/java/com/spring/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/spring/book/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,14 @@
 package com.spring.book.springboot.domain.posts;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 // MyBatis에서 Dao(Data Access Object)라고 불리는 DB Layer, JPA에서는 Repository라고 부른다.
 // Entity 클래스, 기본 Entity Repository는 함께 위치해야 한다. (Entity 클래스는 Repository에 구현된 기능을 통해 역할을 수행하기 때문이다.) - 매우 밀접한 관계
 // 그렇기 때문에 domain 패키지에서 함께 관리한다.
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+  @Query("SELECT p FROM Posts p ORDER BY p.id DESC") // Querydsl
+  List<Posts> findAllDesc();
 }
--- src/main/java/com/spring/book/springboot/web/PostsApiController.java
+++ src/main/java/com/spring/book/springboot/web/PostsApiController.java
@@ -5,6 +5,7 @@ import com.spring.book.springboot.web.dto.PostsResponseDto;
 import com.spring.book.springboot.web.dto.PostsSaveRequestDto;
 import com.spring.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostsApiController {
 
   private final PostsService postsService;
+
+  /*
+    Create: POST
+    Read: GET
+    Update: PUT
+    Delete: DELETE
+  */
 
   @PostMapping("/api/v1/posts")
   public Long save(@RequestBody PostsSaveRequestDto requestDto) {
@@ -31,5 +39,11 @@ public class PostsApiController {
   @GetMapping("/api/v1/posts/{id}")
   public PostsResponseDto findById(@PathVariable Long id) {
     return postsService.findById(id);
+  }
+
+  @DeleteMapping("/api/v1/posts/{id}")
+  public Long delete(@PathVariable Long id) {
+    postsService.delete(id);
+    return id;
   }
 }
